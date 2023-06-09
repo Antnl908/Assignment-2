9b8a9e98fa4bacd11fa041f11c506ec63b5061f3
antn <antnl908@gmail.com>
2023-06-09 10:23:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Threading.Tasks;
+class Assignment2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+        Test.TestVoid();
+    }
+}
+
+class Test
+{
+    public static void TestVoid()
+    {
+        Console.WriteLine("Doing tests!");
+    }
+}
+
